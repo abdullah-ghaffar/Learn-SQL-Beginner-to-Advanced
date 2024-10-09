ef2f0bf68487f54eae70b68380d1c51e83bb328e
Abdullah Ghaffar <abdullahghaffar.work@gmail.com>
2024-10-09 11:53:35 +0500

    2. Select Statement Tutorial

---
+++ 2. Select Statement Tutorial.sql
@@ -0,0 +1,9 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name, last_name, age, age * 2
+FROM parks_and_recreation.employee_demographics;
+
+#PEMDAS
+SELECT DISTINCT first_name, gender
+FROM parks_and_recreation.employee_demographics;
